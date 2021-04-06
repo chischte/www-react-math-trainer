23e954a6f413241221468d8e727ec4a0b12b64ef
michi <chischte@gmx.ch>
2021-04-06 20:36:37 +0200

    BKP

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -25,9 +25,9 @@ export default class LoginPage extends React.Component {
                 <br></br>
                 <h5>INFOS:</h5>
                 <h6>Um alle Trainings nutzen zu können, musst du angemeldet sein.</h6>
-                <h6>Um einen Eintrag in der Highscore machen zu können, musst angemeldet sein.</h6>
-                <h6>Um einer Gruppe beitreten zu können, musst du ebenfalls angemeldet sein.</h6>
-                <h6>Für die Anmeldung benötigst du keine E-Mailadresse.</h6>
+                {/* <h6>Um einen Eintrag in der Highscore machen zu können, musst angemeldet sein.</h6> */}
+                {/* <h6>Um einer Gruppe beitreten zu können, musst du ebenfalls angemeldet sein.</h6> */}
+                <h6>Die anmeldung ist aber kostenlos und du benötigst auch keine E-Mailadresse.</h6>
                 <h6>Die Abkürzung "rpm" bedeutet Rechnungen pro Minute</h6>
                 <h6>•</h6>
                 <h5 style={{color:"blue"}}>----- Viel Spass! -----</h5>
